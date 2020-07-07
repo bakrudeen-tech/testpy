aa44a583392f687e6467a35b94069160ea48ab9e
unknown <ajazchannel@gmail.com>
2020-07-07 15:02:49 +0530

    Printpgm.py

--- Printpgm.py
+++ Printpgm.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 print("this is my seocnd Python program")
-print("this s for master branch")
-=======
-print("this is my third Python program")
-print("this is for testing the secondary branch")
->>>>>>> Secondary
+print("this is secondary")
+
+
